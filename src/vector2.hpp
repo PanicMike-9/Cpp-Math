b280a7790848bdfacb9f339d72a6a587c051b9ca
PanicMike-9 <omsaudager990@gmail.com>
2026-02-09 17:55:08 +0000

    add constructor and length function

--- src/vector2.hpp
+++ src/vector2.hpp
@@ -1,10 +1,23 @@
+#include <cmath>
+
 // header guards
 #ifndef VECTOR2_HPP
 #define VECTOR2_HPP
 
 class Vector2 
 {
+    public:
+        float x;
+        float y;
 
+        // constructor
+        Vector2(float x_c, float y_c) : x(x_c), y(y_c) {}
+
+        // find magnitude
+        float length() const
+        {
+           return std::sqrt(x * x + y * y);  
+        }
 };
 
 #endif
