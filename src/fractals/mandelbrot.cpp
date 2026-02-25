3514dd8d90da220c09ce6bb29d632531b4916797
PanicMike-9 <omsaudager990@gmail.com>
2026-02-25 17:35:55 +0000

    add min and max variables for real and imaginary numbers

--- src/fractals/mandelbrot.cpp
+++ src/fractals/mandelbrot.cpp
@@ -9,6 +9,11 @@ using MB = Mandelbrot; // type alias for MandelBrot
 struct Mandelbrot
 {
     std::vector<int> image;
+
+    double real_min = -2;
+    double real_max = 2;
+    double img_min = -1.5;
+    double img_max = 1.5;
 };
 
 int main() 
