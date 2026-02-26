d1f6fb3d2d4e029bcc7ed9c97c9ef1d7adadf871
PanicMike-9 <omsaudager990@gmail.com>
2026-02-26 17:18:43 +0000

    fix errors

--- src/fractals/mandelbrot.cpp
+++ src/fractals/mandelbrot.cpp
@@ -4,20 +4,31 @@
 #include <vector>
 #include <string>
 
-using MB = Mandelbrot; // type alias for MandelBrot
-
-struct Mandelbrot
+class Mandelbrot
 {
-    std::vector<int> image;
+    public:
+        std::vector<int> image;
 
-    double real_min = -2;
-    double real_max = 2;
-    double img_min = -1.5;
-    double img_max = 1.5;
+        double real_min = -2;
+        double real_max = 2;
+        double img_min = -1.5;
+        double img_max = 1.5;
+
+        int width;
+        int height;
+        const int max_iteration;
+
+        // explicit constructor
+        MandelBrot(int width_c, int height_c, int max_iteration_c)
+            : width(width_c), height(height_c), max_iteration(max_iteration_c) 
+        {
+            image.resize(width * height);
+        }
 };
 
 int main() 
 {
+    std::cout << "working!\n";
     return 0;
 }
 
