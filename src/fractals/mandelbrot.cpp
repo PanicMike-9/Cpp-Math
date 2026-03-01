04c0b970f2609f4c9149d95b8aa6ff706dbd43b6
PanicMike-9 <omsaudager990@gmail.com>
2026-03-01 19:25:28 +0000

    fix typo

--- src/fractals/mandelbrot.cpp
+++ src/fractals/mandelbrot.cpp
@@ -32,7 +32,7 @@ class MandelBrot
             {
                 for(int x = 0; x < width; ++x)
                 {
-                    // map each pixel on the complex plain
+                    // map each pixel on the complex plane
                     std::complex<double> c(real_min + (real_max - real_min) * x / width,
                                            img_min + (img_max - img_min) * y / height);
                     std::complex<double> z = 0;
