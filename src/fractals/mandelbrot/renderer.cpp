09ba1b9c565e296631d534b54161451e7dac68e6
PanicMike-9 <omsaudager990@gmail.com>
2026-03-31 17:01:52 +0000

    add separate compilation with .hpp and .cpp files

--- src/fractals/mandelbrot/renderer.cpp
+++ src/fractals/mandelbrot/renderer.cpp
@@ -5,47 +5,15 @@
 #include <vector>
 #include <string>
 #include <limits>
-#include <chrono> // for each render time
+#include <chrono> 
+#include "renderer.hpp"
 
 // global constexpr
 constexpr double PI  = 3.141592653589793;
 constexpr double TAU = 2.0 * PI;
 
-// multiple fractal formulas
-enum class FractalType
-{
-    mandelbrot,
-    julia_classic,
-    julia_flower,
-    julia_cone,
-    julia_spiral, 
-    julia_snake, 
-    julia_silver_ratio, 
-};
-
-// choose from multiple types
-enum class PaletteType
-{
-    sunset,
-    ocean,
-    fire,
-    neon,
-    blue_green,
-    orange_valley,
-    rainbow,
-    fire_ice,
-    elec_magenta,
-    emerald,
-    elec_blue,
-};
-
-// palette phases struct
-struct Palette
-{
-    double r_phase;
-    double g_phase;
-    double b_phase;
-};
+// type alias
+using MB = Mandelbrot;  
 
 // palette switching
 Palette get_palette(PaletteType type)
@@ -69,194 +37,160 @@ Palette get_palette(PaletteType type)
     return {0.1, 0.4, 0.7};
 }
 
-// camera for the set(centers mandelbrot set)
-struct FractalView
+// explicit constructor
+Mandelbrot::Mandelbrot(int width, int height, int max_iteration, double zoom_factor)
+    : width{width}, height{height}, max_iteration{max_iteration}, zoom_factor{zoom_factor} 
 {
-    double center_real{-0.75}; 
-    double center_img{0.0}; 
-    double real_span{3.5}; 
-};
+    // output image size
+    image.resize(width * height);
 
-class Mandelbrot
+    // base fractal set view values
+    view.center_real = 0.0;
+    view.center_img = 0.0;
+    view.real_span = 3.0 * zoom_factor;
+
+    // set fractal type
+    curr_fractal_type = FractalType::julia_silver_ratio;
+    
+    // set fractal color palette
+    palette = get_palette(PaletteType::rainbow);
+}
+
+// render method
+void Mandelbrot::render_fractal()
 {
-    public:
-        std::vector<int> image;
+    // complex plane real values
+    double real_min = view.center_real - view.real_span/2;
+    double real_max = view.center_real + view.real_span/2;
 
-        // width and height for the fractal set
-        int width;
-        int height;
+    double aspect_ratio = static_cast<double>(height) / static_cast<double>(width);
+    double img_span = view.real_span * aspect_ratio;
 
-        // zoom factor
-        double zoom_factor{0.0};
+    // complex plane imaginary values
+    double img_min = view.center_img - img_span / 2;
+    double img_max = view.center_img + img_span / 2;
 
-        // FractalView for julia set centering(mandelbrot is shifted to left)
-        FractalView julia_view{ 0.00, 0.00, (3.00 * zoom_factor) };
-
-        // complex plane real values
-        double real_min = julia_view.center_real - julia_view.real_span/2;
-        double real_max = julia_view.center_real + julia_view.real_span/2;
-
-        double aspect_ratio = static_cast<double>(height) / static_cast<double>(width);
-        double img_span = julia_view.real_span * aspect_ratio;
-
-        // complex plane imaginary values
-        double img_min = julia_view.center_img - img_span / 2;
-        double img_max = julia_view.center_img + img_span / 2;
-
-        // max amount of iteration
-        const int max_iteration;
-
-        // enum class for multiple formulas
-        FractalType curr_fractal_type = FractalType::julia_silver_ratio;
-
-        // custom palette
-        Palette palette = get_palette(PaletteType::rainbow);
-
-        // explicit constructor
-        Mandelbrot(int width, int height, int max_iteration, double zoom_factor)
-            : width{width}, height{height}, max_iteration{max_iteration}, zoom_factor{zoom_factor} 
+    for(int y = 0; y < height; ++y)
+    {
+        for(int x = 0; x < width; ++x)
         {
-            image.resize(width * height);
-        }
+            // map each pixel on the complex plane
+            std::complex<double> c;
+            std::complex<double> z;
 
-        // render method
-        void render_fractal()
-        {
-            for(int y = 0; y < height; ++y)
+            // compute real and img values
+            double compute_real = (real_min + (real_max - real_min) * static_cast<double>(x) / width);
+            double compute_img = (img_max - (img_max - img_min) * static_cast<double>(y) / height);
+
+            std::complex<double> pixel(compute_real, compute_img);
+
+            // switch fractal formula
+            switch(curr_fractal_type)
             {
-                for(int x = 0; x < width; ++x)
-                {
-                    // map each pixel on the complex plane
-                    std::complex<double> c;
-                    std::complex<double> z;
+                case FractalType::mandelbrot: 
+                    c = pixel;
+                    z = 0;
+                break;
 
-                    // compute real and img values
-                    double compute_real = (real_min + (real_max - real_min) * static_cast<double>(x) / width);
-                    double compute_img = (img_max - (img_max - img_min) * static_cast<double>(y) / height);
+                case FractalType::julia_classic:
+                    z = pixel;
+                    c = std::complex<double>(-0.8, 0.156);
+                break;
 
-                    std::complex<double> pixel(compute_real, compute_img);
+                case FractalType::julia_flower:
+                    z = pixel;
+                    c = std::complex<double>(0.35, 0.35);
+                break;
 
-                    // switch fractal formula
-                    switch(curr_fractal_type)
-                    {
-                        case FractalType::mandelbrot: 
-                            c = pixel;
-                            z = 0;
-                        break;
+                case FractalType::julia_cone:
+                    z = pixel;
+                    c = std::complex<double>(-0.4, 0.6);
+                break;
+                
+                case FractalType::julia_spiral:
+                    z = pixel;
+                    c = std::complex<double>(0.285, 0.01);
+                break;
 
-                        case FractalType::julia_classic:
-                            z = pixel;
-                            c = std::complex<double>(-0.8, 0.156);
-                        break;
-
-                        case FractalType::julia_flower:
-                            z = pixel;
-                            c = std::complex<double>(0.35, 0.35);
-                        break;
-
-                        case FractalType::julia_cone:
-                            z = pixel;
-                            c = std::complex<double>(-0.4, 0.6);
-                        break;
-                        
-                        case FractalType::julia_spiral:
-                            z = pixel;
-                            c = std::complex<double>(0.285, 0.01);
-                        break;
-
-                        case FractalType::julia_snake:
-                            z = pixel;
-                            c = std::complex<double>(-0.835, 0.2321);
-                        break;
-                            
-                        case FractalType::julia_silver_ratio:
-                            z = pixel;
-                            c = std::complex<double>(-0.75, 0.11);
-                        break;
-                    }
-
-                    int iterator = 0;
-
-                    while (std::abs(z) <= 2 && iterator < max_iteration)
-                    {
-                        z = (z * z) + c;
-                        iterator++;
-                    }
-
-                    image[y * width + x] = iterator;
-                }
-            }
-        }
-
-        // save in pgm format
-        void render_gray_scale(const std::string& file_name)
-        {
-            std::ofstream ofs(file_name, std::ios::binary);
-
-            ofs << "P5\n" << width << ' ' << height << "\n255\n";
-
-            for(int i = 0; i < width * height; ++i)
-            {
-                int iteration_num = image[i];
-                unsigned char color = 
-                    static_cast<unsigned char>(255 * iteration_num / max_iteration);
-                ofs << color;
+                case FractalType::julia_snake:
+                    z = pixel;
+                    c = std::complex<double>(-0.835, 0.2321);
+                break;
+                    
+                case FractalType::julia_silver_ratio:
+                    z = pixel;
+                    c = std::complex<double>(-0.75, 0.11);
+                break;
             }
 
-            ofs.close();
-        }
+            int iterator = 0;
 
-        // save in ppm format
-        void render_color(const std::string& file_name)
-        {
-            std::ofstream ofs(file_name, std::ios::binary);
-
-            ofs << "P6\n" << width << ' ' << height << "\n255\n";
-
-            // performance trick
-            double inv_max_iteration = 1.0 / max_iteration;
-
-            for(int i = 0; i < width * height; ++i)
+            while (std::abs(z) <= 2 && iterator < max_iteration)
             {
-                int iteration_num = image[i];
-                unsigned char r, g, b;
-                if(iteration_num == max_iteration)
-                {
-                    // colors the set black
-                    r = 0;
-                    g = 0;                    
-                    b = 0;
-                }
-                else
-                {
-                    // color palette smoothening using std::cos()
-                    double t = iteration_num * inv_max_iteration;
-
-                    r = (int)(255 * (0.5 + 0.5 * std::cos(TAU * (t + palette.r_phase))));
-                    g = (int)(255 * (0.5 + 0.5 * std::cos(TAU * (t + palette.g_phase))));
-                    b = (int)(255 * (0.5 + 0.5 * std::cos(TAU * (t + palette.b_phase))));
-                }
-                ofs.write(reinterpret_cast<char*>(&r), 1);
-                ofs.write(reinterpret_cast<char*>(&g), 1);
-                ofs.write(reinterpret_cast<char*>(&b), 1);
+                z = (z * z) + c;
+                iterator++;
             }
 
-            ofs.close();
+            image[y * width + x] = iterator;
         }
+    }
+}
 
-        /*
-         * Todo *
-         * Add function to create multiple formulas ✅
-         * add time bench mark using chrono ✅
-         * fix the aspect ratio problem(the stretch) ✅ 
-         * add zoom logic ✅
-         * add a struct Palette✅
-         * add a enum class PaletteType✅
-         * add input validation✅
-         */
-};
+// save in pgm format
+void Mandelbrot::render_gray_scale(const std::string& file_name)
+{
+    std::ofstream ofs(file_name, std::ios::binary);
 
-using MB = Mandelbrot; // type alias 
+    ofs << "P5\n" << width << ' ' << height << "\n255\n";
+
+    for(int i = 0; i < width * height; ++i)
+    {
+        int iteration_num = image[i];
+        unsigned char color = 
+            static_cast<unsigned char>(255 * iteration_num / max_iteration);
+        ofs << color;
+    }
+
+    ofs.close();
+}
+
+// save in ppm format
+void Mandelbrot::render_color(const std::string& file_name)
+{
+    std::ofstream ofs(file_name, std::ios::binary);
+
+    ofs << "P6\n" << width << ' ' << height << "\n255\n";
+
+    // performance trick
+    double inv_max_iteration = 1.0 / max_iteration;
+
+    for(int i = 0; i < width * height; ++i)
+    {
+        int iteration_num = image[i];
+        unsigned char r, g, b;
+        if(iteration_num == max_iteration)
+        {
+            // colors the set black
+            r = 0;
+            g = 0;                    
+            b = 0;
+        }
+        else
+        {
+            // color palette smoothening using std::cos()
+            double t = iteration_num * inv_max_iteration;
+
+            r = (int)(255 * (0.5 + 0.5 * std::cos(TAU * (t + palette.r_phase))));
+            g = (int)(255 * (0.5 + 0.5 * std::cos(TAU * (t + palette.g_phase))));
+            b = (int)(255 * (0.5 + 0.5 * std::cos(TAU * (t + palette.b_phase))));
+        }
+        ofs.write(reinterpret_cast<char*>(&r), 1);
+        ofs.write(reinterpret_cast<char*>(&g), 1);
+        ofs.write(reinterpret_cast<char*>(&b), 1);
+    }
+
+    ofs.close();
+}
 
 void output()
 {
@@ -302,7 +236,7 @@ void output()
               << "real_max: "  << fractal.real_max    << '\n'
               << "img_min: "   << fractal.img_min     << '\n'
               << "img_max: "   << fractal.img_max     << '\n'
-              << "real_span: " << fractal.julia_view.real_span   << '\n';
+              << "real_span: " << fractal.view.real_span   << '\n';
 }
 
 void output_color()
@@ -368,7 +302,7 @@ void output_color()
               << "real_max: "  << fractal.real_max    << '\n'
               << "img_min: "   << fractal.img_min     << '\n'
               << "img_max: "   << fractal.img_max     << '\n'
-              << "real_span: " << fractal.julia_view.real_span   << '\n';
+              << "real_span: " << fractal.view.real_span   << '\n';
 }
 
 int main() 
