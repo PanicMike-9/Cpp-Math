24a4f44aa58c5553754ba656f08edf4d803eba03
PanicMike-9 <omsaudager990@gmail.com>
2026-04-01 14:27:18 +0000

    add output and generate functions in frames files, clean up main

---
+++ src/fractals/mandelbrot/frames.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "renderer.hpp"
+
+// type alias
+using MB = Mandelbrot;  
+
+// function definitions
+void generate_frames();
+void output_color();
+void output_gray_scale();
