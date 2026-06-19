5a70cf27904243c773d4ddb52bfc6a550fdbad8e
PanicMike-9 <omsaudager990@gmail.com>
2026-06-19 13:38:51 +0000

    Refactor(vector2): remove assert from functions and operator overloads

--- src/linear_algebra/vector2.hpp
+++ src/linear_algebra/vector2.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cmath>
-#include <cassert> // for assert
 
 namespace vmath
 {
@@ -45,9 +44,8 @@ class Vector2
         inline Vector2 normalize() const
         {
             float mag = length();
-            assert(mag > 0.0f && "Vector2: Division by zero!");
-
             float inv = 1.0f / mag;
+
             return Vector2(x * inv, y * inv);
         }
 
@@ -135,13 +133,11 @@ class Vector2
         // operator divide
         constexpr Vector2 operator/(const Vector2& other) const
         {
-            assert(other.x != 0.0f && other.y != 0.0f && "Vector2: Division by zero!");
             return Vector2{x / other.x, y / other.y};
         }
 
         constexpr Vector2 operator/(float scalar) const
         {
-            assert(scalar != 0.0f && "Vector2: Division by zero!");
             return Vector2{x / scalar, y / scalar};
         }
 
@@ -178,7 +174,6 @@ class Vector2
         // assignment operator /=
         constexpr Vector2& operator/=(const Vector2& other)
         {
-            assert(other.x != 0.0f && other.y != 0.0f && "Vector2: Division by zero!");
             x /= other.x;
             y /= other.y;
             return *this;
@@ -186,7 +181,6 @@ class Vector2
 
         constexpr Vector2& operator/=(float scalar)
         {
-            assert(scalar != 0.0f && "Vector2: Division by zero!");
             float inv = 1.0f / scalar;
             x *= inv;
             y *= inv;
