29f685241ccf3edc7786c25fae1b4e80f20f1bd0
PanicMike-9 <omsaudager990@gmail.com>
2026-06-20 13:27:13 +0000

    Feat: add vector3 with basic functions, namespace and constructor

---
+++ src/linear_algebra/vector3.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cmath>
+
+namespace geom
+{
+
+const float PI = 3.14159265358979323846f;
+
+// convert radians to degrees
+inline float rad_to_deg(float radians)
+{
+    return radians * (180.0f / PI);
+}
+
+// convert degrees to radians
+inline float deg_to_rad(float degrees)
+{
+    return degrees * (PI / 180.0f);
+}
+ 
+class Vector3
+{
+    public:
+        float x;
+        float y;
+        float z;
+
+        // constructor
+        Vector3(float x, float y, float z) : x{x}, y{y}, z{z} {}
+};
+
+} // namespace geom
