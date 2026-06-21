c55d80b854ac82d9cade2106f7a7a11bc15e3154
PanicMike-9 <omsaudager990@gmail.com>
2026-06-21 14:51:38 +0000

    Feat(vector3): add distance, dot functions as free functions, add operator overload for '-' for scalar and vector

--- src/linear_algebra/vector3.hpp
+++ src/linear_algebra/vector3.hpp
@@ -18,7 +18,7 @@ inline float deg_to_rad(float degrees)
 {
     return degrees * (PI / 180.0f);
 }
- 
+
 class Vector3
 {
     public:
@@ -27,10 +27,61 @@ class Vector3
         float z;
 
         // default constructor
-        Vector3() : x{0.0f}, y{0.0f}, z{0.0f} {}
+        constexpr Vector3() : x{0.0f}, y{0.0f}, z{0.0f} {}
 
         // constructor
-        Vector3(float x, float y, float z) : x{x}, y{y}, z{z} {}
+        constexpr Vector3(float x, float y, float z) : x{x}, y{y}, z{z} {}
+
+        // find magnitude squared
+        inline float length_squared() const
+        {
+            return (x * x + y * y + z * z);
+        }
+
+        // find magnitude 
+        inline float length() const
+        {
+            return std::sqrt(length_squared());
+        }
+
+        // find unit vector/normalize vector
+        inline Vector3 normalize() const
+        {
+            float mag = length();
+            float inv = 1.0f / mag;
+
+            return Vector3(x * inv, y * inv, z * inv);
+        }
+
+        // ** operator overloads ** //
+
+        constexpr Vector3 operator-(float scalar) const
+        {
+            return Vector3{x - scalar, y - scalar, z - scalar};
+        }
+
+        constexpr Vector3 operator-(const Vector3 &other) const
+        {
+            return Vector3{x - other.x, y - other.y, z - other.z};
+        }
 };
+
+// find squared distance of two vectors
+inline float distance_squared(const Vector3 &a, const Vector3 &b) 
+{
+    return (a - b).length_squared();
+}
+
+// find distance of two vectors
+inline float distance(const Vector3 &a, const Vector3 &b) 
+{
+    return (a - b).length();
+}
+
+// find dot product
+inline float dot(const Vector3 &a, const Vector3 &b) 
+{
+    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+}
 
 } // namespace geom
