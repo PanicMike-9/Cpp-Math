9fc073f35ddb00015416b79ea562075c6fc923da
PanicMike-9 <omsaudager990@gmail.com>
2026-06-24 15:01:10 +0000

    Refactor(frames): remove previous comments

--- src/fractals/mandelbrot/frames.hpp
+++ src/fractals/mandelbrot/frames.hpp
@@ -5,11 +5,4 @@
 using MB = Mandelbrot;  
 
 // function definitions
-void generate_zoom();
-void generate_parametric();
-
-/*
- * right now these functions aren't required
-void output_color();
-void output_gray_scale();
-*/
+void generate_frames();
