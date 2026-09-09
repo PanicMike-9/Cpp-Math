8b9d3329a5a00a73d76d66e206dbadcd76031015
PanicMike-9 <omsaudager990@gmail.com>
2026-09-09 19:36:58 +0000

    fix(simd): replace constexpr with inline, replace avx to sse for 128-byte floats, add simd namespace

--- src/simd/simd_multiply_floats.hpp
+++ src/simd/simd_multiply_floats.hpp
@@ -4,7 +4,10 @@
 #include <xmmintrin.h>
 #include <immintrin.h> // works for GCC, Clang and MSVC
 
-constexpr void avx_multiply_floats_128(float a[4], float b[4], float result[4])
+namespace simd
+{
+
+inline void sse_multiply_floats_128(float a[4], float b[4], float result[4])
 {
     __m128 arr_a = _mm_loadu_ps(a);
     __m128 arr_b = _mm_loadu_ps(b);
@@ -13,7 +16,7 @@ constexpr void avx_multiply_floats_128(float a[4], float b[4], float result[4])
     _mm_storeu_ps(result, calculate);
 }
 
-constexpr void avx2_multiply_floats_256(float a[8], float b[8], float result[8])
+inline void avx2_multiply_floats_256(float a[8], float b[8], float result[8])
 {
     __m256 arr_a = _mm256_loadu_ps(a);
     __m256 arr_b = _mm256_loadu_ps(b);
@@ -21,4 +24,6 @@ constexpr void avx2_multiply_floats_256(float a[8], float b[8], float result[8])
     __m256 calculate = _mm256_mul_ps(arr_a, arr_b);
     _mm256_storeu_ps(result, calculate);
 }
+
+}// namespace simd
 
