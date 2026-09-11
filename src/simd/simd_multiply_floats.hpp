1e419b667c7666e4546f0617fcbae0a292bad258
PanicMike-9 <omsaudager990@gmail.com>
2026-09-11 20:36:41 +0000

    feat(simd): add sse and avx2 float array mulitplication

--- src/simd/simd_multiply_floats.hpp
+++ src/simd/simd_multiply_floats.hpp
@@ -7,14 +7,29 @@
 namespace simd
 {
 
+// ---- SSE 128-byte Functions ----
+    
 inline void sse_multiply_floats_128(float a[4], float b[4], float result[4])
 {
-    __m128 arr_a = _mm_loadu_ps(a);
+    __m128 arr_a  = _mm_loadu_ps(a);
     __m128 arr_b = _mm_loadu_ps(b);
 
     __m128 calculate = _mm_mul_ps(arr_a, arr_b);
     _mm_storeu_ps(result, calculate);
 }
+
+inline void sse_vec_multiply_scalar_128(float a[], std::size_t n, float scalar)
+{
+    const __m128 arr_scalar = _mm_set1_ps(scalar);
+    for (std::size_t i {}; i < n; i += 4)
+    {
+        __m128 arr_a = _mm_loadu_ps(&a[i]);
+        __m128 calculate = _mm_mul_ps(arr_a, arr_scalar);
+        _mm_store_ps(&a[i], calculate);
+    }
+}
+
+// ---- AVX2 256-byte Functions ----
 
 inline void avx2_multiply_floats_256(float a[8], float b[8], float result[8])
 {
@@ -23,6 +38,17 @@ inline void avx2_multiply_floats_256(float a[8], float b[8], float result[8])
 
     __m256 calculate = _mm256_mul_ps(arr_a, arr_b);
     _mm256_storeu_ps(result, calculate);
+}
+
+inline void avx2_vec_multiply_scalar_256(float a[], std::size_t n, float scalar)
+{
+    const __m256 arr_scalar = _mm256_set1_ps(scalar);
+    for (std::size_t i {}; i < n; i += 8)
+    {
+        __m256 arr_a = _mm256_loadu_ps(&a[i]);
+        __m256 calculate = _mm256_mul_ps(arr_a, arr_scalar);
+        _mm256_store_ps(&a[i], calculate);
+    }
 }
 
 }// namespace simd
