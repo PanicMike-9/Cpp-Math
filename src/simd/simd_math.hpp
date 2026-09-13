9cd955144433bee4201cf01ad119a43b2b109998
PanicMike-9 <omsaudager990@gmail.com>
2026-09-13 18:58:25 +0000

    refactor(simd_math): Standardize API naming and improve signatures - Remove sse and avx2 prefixes from function names, prioritize 128 and 256 bit suffixes - Rename vec_ to array_ for dynamic operations - Replace array paramaters with float* and mark functions with noexcept

--- src/simd/simd_math.hpp
+++ src/simd/simd_math.hpp
@@ -7,9 +7,9 @@
 namespace simd
 {
 
-// ---- SSE 128-byte Functions ----
+// ---- SSE 128-bit Functions ----
     
-inline void sse_multiply_floats_128(float a[4], float b[4], float result[4])
+inline void multiply_floats_128(const float a[4], const float b[4], float result[4])
 {
     __m128 arr_a  = _mm_loadu_ps(a);
     __m128 arr_b = _mm_loadu_ps(b);
@@ -18,7 +18,7 @@ inline void sse_multiply_floats_128(float a[4], float b[4], float result[4])
     _mm_storeu_ps(result, calculate);
 }
 
-inline void sse_vec_multiply_scalar_128(float a[], std::size_t n, float scalar)
+inline void array_multiply_scalar_128(float* a, const std::size_t n, const float scalar) noexcept
 {
     const __m128 arr_scalar = _mm_set1_ps(scalar);
     std::size_t i {0};
@@ -28,11 +28,35 @@ inline void sse_vec_multiply_scalar_128(float a[], std::size_t n, float scalar)
         __m128 calculate = _mm_mul_ps(arr_a, arr_scalar);
         _mm_storeu_ps(&a[i], calculate);
     }
+
+    // process remainder values
+    for (; i < n; ++i)
+    {
+        a[i] *= scalar;
+    }
 }
 
-// ---- AVX2 256-byte Functions ----
+inline void array_add_scalar_128(float* a, const std::size_t n, const float scalar) noexcept
+{
+    const __m128 arr_scalar = _mm_set1_ps(scalar);
+    std::size_t i {0};
+    for (; i + 3 < n; i += 4)
+    {
+        __m128 arr_a = _mm_loadu_ps(&a[i]);
+        __m128 calculate = _mm_add_ps(arr_a, arr_scalar);
+        _mm_storeu_ps(&a[i], calculate);
+    }
 
-inline void avx2_multiply_floats_256(float a[8], float b[8], float result[8])
+    // process remainder values
+    for (; i < n; ++i)
+    {
+        a[i] += scalar;
+    }
+}
+
+// ---- AVX2 256-bit Functions ----
+
+inline void multiply_floats_256(const float a[8], const float b[8], float result[8])
 {
     __m256 arr_a = _mm256_loadu_ps(a);
     __m256 arr_b = _mm256_loadu_ps(b);
@@ -41,7 +65,7 @@ inline void avx2_multiply_floats_256(float a[8], float b[8], float result[8])
     _mm256_storeu_ps(result, calculate);
 }
 
-inline void avx2_vec_multiply_scalar_256(float a[], const std::size_t n, const float scalar)
+inline void array_multiply_scalar_256(float* a, const std::size_t n, const float scalar) noexcept
 {
     const __m256 arr_scalar = _mm256_set1_ps(scalar);
     std::size_t i {0};
@@ -57,6 +81,25 @@ inline void avx2_vec_multiply_scalar_256(float a[], const std::size_t n, const f
     for (; i < n; ++i)
     {
         a[i] *= scalar;
+    }
+}
+
+inline void array_add_scalar_256(float* a, const std::size_t n, const float scalar) noexcept
+{
+    const __m256 arr_scalar = _mm256_set1_ps(scalar);
+    std::size_t i {0};
+    for (; i + 7 < n; i += 8)
+    {
+        __m256 arr_a = _mm256_loadu_ps(&a[i]);
+
+        __m256 calculate = _mm256_add_ps(arr_a, arr_scalar);
+        _mm256_storeu_ps(&a[i], calculate);
+    }
+
+    // process remainder values
+    for (; i < n; ++i)
+    {
+        a[i] += scalar;
     }
 }
 
