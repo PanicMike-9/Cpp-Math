c01a6ecfa0a692fd569e1f7603b6692aa1baac9e
PanicMike-9 <omsaudager990@gmail.com>
2026-09-18 18:34:41 +0000

    refactor(main): move tests into helper functions

--- src/simd/main.cpp
+++ src/simd/main.cpp
@@ -1,24 +1,120 @@
 #include <print>
 #include "simd_math.hpp"
 
-void cpu_feature_check()
+// inline void cpu_feature_check(); // Warning: Requires fmt/core.h & fmt/color.h
+// quick check at runtime
+inline bool supports_avx2();
+
+inline void test_256f_scalar_multiplication();
+inline void test_256f_multiplication();
+inline void test_128f_addition();
+inline void test_128f_multiplication();
+
+int main()
+{
+    test_128f_addition();
+    test_256f_multiplication();          
+    test_128f_multiplication();
+    test_256f_scalar_multiplication();
+}
+
+inline void test_256f_scalar_multiplication()
+{
+    alignas(32) float a[8] {8.0f, 16.0f, 32.0f, 40.0f, 48.0f, 56.0f, 64.0f, 72.0f};
+    alignas(32) float b[8] {19.0f, 27.0f, 43.0f, 51.0f, 59.0f, 67.0f, 75.0f, 83.0f};
+    const float scalar {100.0f};
+
+    // causes core dumped, stack smashing error with GCC when array size is not known at compile time, no issues with Clang
+    simd::array_multiply_scalar_256(a, 8, scalar);
+
+    std::println("256-bit float 'a' array multiplication with const float scalar: {}", scalar);
+    for (const float i : a)
+    {
+        std::print("{} ", i);
+    }
+    std::println();
+
+    simd::array_multiply_scalar_256(b, 8, scalar);
+
+    std::println("256-bit float 'b' array multiplication with const float scalar: {}", scalar);
+    for (const float i : b)
+    {
+        std::print("{} ", i);
+    }
+    std::println();
+}
+
+inline void test_256f_multiplication()
+{
+    // alignas 32 = 8 floats * 4 
+    alignas(32) float a[8] {8.0f, 16.0f, 32.0f, 40.0f, 48.0f, 56.0f, 64.0f, 72.0f};
+    alignas(32) float b[8] {8.0f, 16.0f, 32.0f, 40.0f, 48.0f, 56.0f, 64.0f, 72.0f};
+    alignas(32) float result[8];
+
+    simd::multiply_floats_256(a, b, result);
+
+    std::println("256-bit float multiplication");
+    for (const float i : result)
+    {
+        std::print("{} ", i);
+    }
+    std::println();
+}
+
+inline void test_128f_addition()
+{
+    alignas(16) float a[4] {88.0f, 1600.0f, 44.0f, 4000.49f};
+    alignas(16) float b[4] {7.0f, 14.0f, 302.0f, 21.98f};
+    alignas(16) float result[4];
+    simd::add_floats_128(a, b, result);
+
+    std::println("128-bit float addition");
+    for (const float i : result)
+    {
+        std::print("{} ", i);
+    }
+    std::println();
+}
+
+inline void test_128f_multiplication()
+{
+    // alignas 16 = 4 floats * 4 
+    alignas(16) float a[4] {8.0f, 16.0f, 32.0f, 40.0f};
+    alignas(16) float b[4] {8.0f, 16.0f, 32.0f, 40.0f};
+    alignas(16) float result[4];
+
+    simd::multiply_floats_128(a, b, result);
+
+    std::println("128-bit float multiplication");
+    for (const float i : result)
+    {
+        std::print("{} ", i);
+    }
+    std::println();
+}
+
+#if 0
+#include <fmt/core.h>
+#include <fmt/color.h>
+
+inline void cpu_feature_check() 
 {
 #if defined(__GNUC__) || defined(__clang__)
     __builtin_cpu_init();
 
-    std::println("=== Hardware SIMD Support ===");
-    std::println("SSE4.1:  {}", __builtin_cpu_supports("sse4.1"));
-    std::println("SSE4.2:  {}", __builtin_cpu_supports("sse4.2"));
-    std::println("AVX:     {}", __builtin_cpu_supports("avx"));
-    std::println("AVX2:    {}", __builtin_cpu_supports("avx2"));
-    std::println("AVX512F: {}", __builtin_cpu_supports("avx512f"));
+    fmt::print(fg(fmt::color::green), "=== Hardware SIMD Support ===\n");
+    fmt::print(fg(fmt::color::green), "SSE4.1:  {}\n", __builtin_cpu_supports("sse4.1"));
+    fmt::print(fg(fmt::color::green), "SSE4.2:  {}\n", __builtin_cpu_supports("sse4.2"));
+    fmt::print(fg(fmt::color::green), "AVX:     {}\n", __builtin_cpu_supports("avx"));
+    fmt::print(fg(fmt::color::green), "AVX2:    {}\n", __builtin_cpu_supports("avx2"));
+    fmt::print(fg(fmt::color::green), "AVX512F: {}\n", __builtin_cpu_supports("avx512f"));
 
 #else 
-    std::println("!!! Non-GCC/Clang compiler detected (SIMD check skipped) !!!");
+    fmt::print(fg(fmt::color::red), "\n!!! Non-GCC/Clang compiler detected (SIMD check skipped) !!!\n");
 #endif
 }
+#endif
 
-// quick check at runtime
 inline bool supports_avx2()
 {
 #if defined(__GNUC__) || defined(__clang__)
@@ -27,61 +123,4 @@ inline bool supports_avx2()
 #else
     return false;
 #endif
-}
-
-int main()
-{
-    // alignas 16 = 4 floats * 4 
-    alignas(16) float a_128[4] {8.0f, 16.0f, 32.0f, 40.0f};
-    alignas(16) float b_128[4] {8.0f, 16.0f, 32.0f, 40.0f};
-    alignas(16) float result_128[4];
-
-    simd::multiply_floats_128(a_128, b_128, result_128);
-
-    std::println("SSE 128-bit float multiplication");
-    for (const float i : result_128)
-    {
-        std::print("{} ", i);
-    }
-    std::println();
-
-    // alignas 32 = 8 floats * 4 
-    alignas(32) float a_256[8] {8.0f, 16.0f, 32.0f, 40.0f, 48.0f, 56.0f, 64.0f, 72.0f};
-    alignas(32) float b_256[8] {8.0f, 16.0f, 32.0f, 40.0f, 48.0f, 56.0f, 64.0f, 72.0f};
-    alignas(32) float result_256[8];
-
-    simd::multiply_floats_256(a_256, b_256, result_256);
-
-    std::println("AVX2 256-bit float multiplication");
-    for (const float i : result_256)
-    {
-        std::print("{} ", i);
-    }
-    std::println();
-
-    alignas(32) float a_sc_256[8] {8.0f, 16.0f, 32.0f, 40.0f, 48.0f, 56.0f, 64.0f, 72.0f};
-    alignas(32) float b_sc_256[8] {19.0f, 27.0f, 43.0f, 51.0f, 59.0f, 67.0f, 75.0f, 83.0f};
-    const float scalar {100.0f};
-    //alignas(32) const std::size_t size_of_a {sizeof(a_scalar_256) / sizeof(a_scalar_256[0])};
-
-    // causes core dumped, stack smashing error with GCC when array size is not known at compile time, no issues with Clang
-    simd::array_multiply_scalar_256(a_sc_256, 8, scalar);
-
-    std::println("AVX2 256-bit float 'a' array multiplication with const float scalar: {}", scalar);
-    for (const float i : a_sc_256)
-    {
-        std::print("{} ", i);
-    }
-    std::println();
-
-    simd::array_multiply_scalar_256(b_sc_256, 8, scalar);
-
-    std::println("AVX2 256-bit float 'b' array multiplication with const float scalar: {}", scalar);
-    for (const float i : b_sc_256)
-    {
-        std::print("{} ", i);
-    }
-    std::println();
-
-    //cpu_feature_check();
 }
